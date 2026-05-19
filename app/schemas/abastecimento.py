7aba04f965ad544cbce24eee5fcfa0f976fd6f8c
LuizzDutra <luizzidutra@gmail.com>
2026-05-18 23:13:44 -0300

    datetime aware

--- app/schemas/abastecimento.py
+++ app/schemas/abastecimento.py
@@ -1,8 +1,8 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from decimal import Decimal
 from typing import Annotated
 
-from pydantic import AfterValidator, BaseModel, Field
+from pydantic import AfterValidator, BaseModel, Field, ConfigDict, field_validator, AwareDatetime
 
 from app.core.validators.cpf import validate as cpf_validator
 from app.schemas.enums import TipoCombustivel
@@ -10,11 +10,23 @@ from app.schemas.enums import TipoCombustivel
 CPF = Annotated[str, AfterValidator(cpf_validator)]
 
 class AbastecimentoSchema(BaseModel):
-    id_post: int
-    data_hora: datetime = Field(le=datetime.now())
+    id_posto: int
+    data_hora: AwareDatetime
     tipo_combustivel: TipoCombustivel
     preco_por_litro: Decimal = Field(gt=0)
     volume_abastecido: Decimal = Field(gt=0)
     cpf_motorista: CPF
 
+    @field_validator("data_hora")
+    @classmethod
+    def data_hora_nao_pode_ser_futura(cls, data: datetime) -> datetime:
+        if data > datetime.now(timezone.utc):
+            raise ValueError("data_hora não pode ser no futuro")
+        return data
 
+
+class AbastecimentoModel(AbastecimentoSchema):
+    id: int
+    improper_data: bool
+
+    model_config = ConfigDict(from_attributes=True)
