6d63b2394b9893606175a6d706d223251330e997
LuizzDutra <luizzidutra@gmail.com>
2026-05-19 12:30:15 -0300

    Schema no retorno

--- app/routers/motoristas.py
+++ app/routers/motoristas.py
@@ -2,14 +2,15 @@ from fastapi import APIRouter, status
 
 from app.core.db import SessionDep
 from app.repositories.motoristas import get_abastecimentos_motorista
-from app.schemas.abastecimento import CPF
+from app.schemas.abastecimento import CPF, AbastecimentoModel
 
 router = APIRouter(prefix="/motoristas")
 
 
 @router.get("/{cpf}/historico", status_code=status.HTTP_200_OK)
-async def get_motorista_cpf(cpf: CPF, session: SessionDep):
+async def get_motorista_cpf(cpf: CPF, session: SessionDep) -> list[AbastecimentoModel]:
 
     result = await get_abastecimentos_motorista(session, cpf)
+    result = [AbastecimentoModel.model_validate(m) for m in result]
 
     return result
